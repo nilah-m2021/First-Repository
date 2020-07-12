f26f89324aaeed8c66b6399bdcc90e02cabf1fc0
nilah-m2021 <67973031+nilah-m2021@users.noreply.github.com>
2020-07-12 13:22:50 -0500

    Strings Playground 1st Commit
    Strings Playground 1st Commit

---
+++ Strings.playground/Pages/Exercise-Making a List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ ## Exercise: Making a List
+ 
+ Lists are great. Here are some constants describing some of the things you’ve learned about strings so far:
+ */
+let constants = "Declaring string constants"
+let unicode = "Unicode characters (😎)"
+let combining = "Combining strings using +"
+let interpolation = "String interpolation (aka Fill in the Blanks)"
+let escaping = "Escape characters for \"special powers\""
+let newline = "Making new lines"
+/*:
+ - experiment: Make a new string constant that is a list of the things you’ve learned, with each entry on a new line. Make sure you add the result to the playground page so that you can see the list properly.
+ */
+let myName = "Nilah"
+let 😂🐻🐼 = " Marshall"
+let myFullName = myName + 😂🐻🐼
+let activity = "code"
+let fullSentence = "\(myFullName) likes to \(activity)"
+let nilahQuote = "Nilah said \"she likes to code\""
+let someNew = "She \n likes to \n have fun"
+
+
+//:
+//:[Previous](@previous)  |  page 13 of 16  |  [Next: Exercise: A Restaurant](@next)
