b4151d2579328240008bb16e0130c24951a2a868
nilah-m2021 <67973031+nilah-m2021@users.noreply.github.com>
2020-07-12 11:50:08 -0500

    Playground Basics 1st Commit
    Playground Basics 1st Commit

---
+++ PlaygroundBasics.playground/Pages/Calculations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: ## Calculations
+//: You can use a playground like a calculator. Type in the math problem you want solved and the results sidebar shows the answer, or the _result_, of the calculation.
+//:
+//: You can use the plus `+` and minus `-` signs for addition and subtraction, just as you’d expect:
+12 + 46 + 2356
+
+10230 - 3780
+
+567 + 78
+
+23 - 167
+
+//: For multiplication, use the asterisk `*` :
+3 * 1012
+
+456 * 23
+
+1209 * 345
+//: For division, use the slash `/` :
+24 / 6
+
+678 / 25
+
+906799 / 5609
+
+//: For grouping, use parenthesis `( )` :
+(3900 - 5340 + 210) / 3
+(6748 * 2333) + 1788
+(-162 / 2) - 583
+//: - experiment:\
+//: Practice using the playground like a calculator.
+//: Edit some of the existing expressions and add a few of your own.
+//: Notice that every time you make a change the results are updated in the sidebar.
+//:
+//: Next, learn how programmers can leave notes in code.
+//:
+//:[Previous](@previous)  |  page 3 of 7  |  [Next: Comments](@next)
